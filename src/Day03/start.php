c4db1d722bcecf7c123372cda828d5fd5037755a
flv12 <26801813+flv12@users.noreply.github.com>
2024-12-04 23:50:45 +0100

    Add day 03 part 2 puzzle

--- src/Day03/start.php
+++ src/Day03/start.php
@@ -5,19 +5,32 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use Advent2024\Common\InputFileReader;
 
 $globalSum = 0;
+$instructionsEnabled = true;
 
 $inputFileReader = new InputFileReader();
 foreach ($inputFileReader->read(__DIR__ . '/input.txt') as $line) {
     $sum = 0;
     $matches = [];
 
-    preg_match_all("/mul\((\d{1,3}),(\d{1,3})\)/", $line, $matches);
+    preg_match_all("/mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))/", $line, $matches);
     for ($i = 0; $i < count($matches[0]); $i++) {
         $num1 = $matches[1][$i];
         $num2 = $matches[2][$i];
+        $doCall = $matches[3][$i];
+        $dontCall = $matches[4][$i];
+
+        if ($doCall === 'do()') {
+            $instructionsEnabled = true;
+        }
+
+        if ($dontCall === "don't()") {
+            $instructionsEnabled = false;
+        }
 
         echo sprintf("[DEBUG] %s * %s\n", $num1, $num2);
-        $sum += ($num1 * $num2);
+        if ($instructionsEnabled && is_numeric($num1) && is_numeric($num2)) {
+            $sum += ($num1 * $num2);
+        }
     }
     $globalSum += $sum;
 }
